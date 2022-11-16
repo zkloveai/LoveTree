5fb5a4153e4c1d5614b0769b078f1af7098082b9
周凯 <41566314+zkloveai@users.noreply.github.com>
2022-11-16 19:30:17 +0800

    Update functions.js

--- index_files/functions.js
+++ index_files/functions.js
@@ -37,21 +37,3 @@ $(window).resize(function() {
 	};
 })(jQuery);
 
-function timeElapse(date){
-	var seconds = (+ new Date() - date.getTime()) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = Math.floor(seconds % 60);
-	seconds = (seconds + "").replace(/\.\d*/, "");
-	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒";
-	$("#clock").html(result);
-}
